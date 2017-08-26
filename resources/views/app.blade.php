09a2d77141365991149da027acfc1c25b035c377
Heri Julianto Situmorang <komblog@users.noreply.github.com>
2017-08-26 10:06:40 +0700

    Add files via upload

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<title>List User</title>
-
+	<meta name="csrf-token" content="{{ csrf_token() }}">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
@@ -13,7 +13,7 @@
 		@yield('content')
 	</div>
 	<script src="https://code.jquery.com/jquery-3.2.1.js"></script>
-	<script src="{{ URL::asset('js/script.js') }}"></script>
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twbs-pagination/1.3.1/jquery.twbsPagination.min.js"></script> <script src="{{ URL::asset('js/script.js') }}"></script>
   	<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"></script>
   	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
